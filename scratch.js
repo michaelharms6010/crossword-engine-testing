ab2e7d4f794136305b25ad7d375eb4bd853bbc9b
Michael Harms <michael.harms6010@gmail.com>
2020-03-29 09:30:02 -0500

    starts line checker

--- scratch.js
+++ scratch.js
@@ -20,3 +20,28 @@ console.log("Your coffee is empty!")
 // Grid adjacency test - call it "inline" - 
 // interprets selection direciton, 
 // resets grid if new selection doesn't fit the direction
+
+
+// checks if the gridpoitns in arr2 are in line with arr1
+function checkLine(arr1, arr2) {
+    if (arr1.length === 0) return true
+    else if (arr1.length === 1) {
+        // check if it's one away on row
+        if (Math.abs((arr1[0][0] - arr2[0])) === 1 ) {
+            if (Math.abs((arr1[0][1] - arr2[1])) === 1 ) {
+                return "diagonal"
+            }
+            return "column"
+        }
+        if (Math.abs((arr1[0][1] - arr2[1])) === 1 ) {
+            return "row"
+        }
+    } else if (arr1.length > 1) {
+
+
+
+    }
+
+
+}
+
