3afa8ba532e9b11e2307a6b33b05a551ed30a45c
Michael Harms <michael.harms6010@gmail.com>
2020-04-01 16:23:16 -0500

    writes ascending tester scratch

--- scratch.js
+++ scratch.js
@@ -1,16 +1,25 @@
-function prompt(question, callback) {
-    var stdin = process.stdin,
-        stdout = process.stdout;
-
-    stdin.resume();
-    stdout.write(question + "\n");
-
-    stdin.once('data', function (data) {
-        callback(data.toString().trim());
-    });
+function ascending(str) {
+    let len = 1;
+    while (len <= str.length / 2) {
+        let numbers = [];
+        for (i = 0; i < str.length; i+=len) {
+            numbers.push(Number(str.slice(i, i+len)))
+            
+        }
+        console.log(numbers)
+        len += 1;
+        if (testAscending(numbers)) return true
+    }
+    return false
 }
 
-prompt("What's your name?", input => {
-    console.log("Your name is " + input)
-    process.exit()})
+function testAscending(arr) {
+    if (arr.length > 1) {
+        for (i = 1; i < arr.length; i++) {
+            if (arr[i] !== arr[i-1] + 1) return false
+        }
+        return true
+    }
+}
 
+console.log(ascending("123124"))
